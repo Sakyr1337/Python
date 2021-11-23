ec993b7b69de32881fbff1f37811a737a75bd8ee
Sakyr1337 <94933977+Sakyr1337@users.noreply.github.com>
2021-11-24 10:05:22 +1300

    Add files via upload

---
+++ digitech.py
@@ -0,0 +1,15 @@
+print(5)
+
+n1 = 20
+n2 = 20
+
+answer = n1 + n2
+
+print(answer)
+
+name = input("What is your name?")
+print("Hello", name + "!")
+
+age = input("What is your age?")
+age = int(age)
+print("You are", str(age), "years old.")
